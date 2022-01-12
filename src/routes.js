8b56df4fcf78f0904b382bbfa43ca7f9c18f1297
Erick Etiene <333.erick@gmail.com>
2022-01-12 00:40:43 -0300

    Melhorias de Layout e refatoração do HTML

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,41 @@
 const express = require('express')
 const routes = express.Router()
 
-const clientes = []
+const consultas = [
+    {
+        id_consulta: 1,
+        nome: 'Erick Etiene',
+        procedimento: 'Limpeza',
+        horario: '2022-02-01T14:00',
+        valor: 100,
+        createdAt: Date.now()
+    },
+    {
+        id_consulta: 2,
+        nome: 'Taís Rocha',
+        procedimento: 'Aparelho',
+        horario: '2022-02-01T14:00',
+        valor: 80,
+        createdAt: Date.now()
+    }
+]
 
 //request, response
-routes.get('/', (req, res)=> res.sendFile(__dirname + "/views/index.html"))
+routes.get('/', (req, res)=> res.sendFile(__dirname + "/views/index.html", {consultas}))
+routes.get('/consultas', (req, res)=> res.sendFile(__dirname + "/views/consultas.html"))
+routes.get('/historico', (req, res)=> res.sendFile(__dirname + "/views/historico.html"))
 routes.post('/', (req, res)=>{
-    clientes.push(req.body)
-    return res.redirect('/')
+
+    const consulta = req.body
+    const lastId = consultas[consultas.length - 1]?.id_consulta || 1
+    consultas.push({
+        id_consulta: lastId + 1,
+        nome: consulta.nome,
+        procedimento: consulta.procedimento,
+        horario: consulta.horario,
+        valor: consulta.valor,
+        createdAt: Date.now() //atribuindo data de criação da consulta
+    })
 })
 
 module.exports = routes;
